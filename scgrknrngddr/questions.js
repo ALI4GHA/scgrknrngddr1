6fffb82d7134e2ab90e4b7b9d8d2c6bb5eeae92d
ALI4GHA <aliagasadiqli27@gmail.com>
2026-05-01 16:36:39 +0400

    Revise financial scenarios in questions.js
    Updated financial choices and scenarios in the questions array.

--- scgrknrngddr/questions.js
+++ scgrknrngddr/questions.js
@@ -19,9 +19,26 @@ const questions = {
         { left: "Həmişə metroda getmək", right: "Həmişə piyada getmək" }
     ],
     pul: [
-        { left: "İndi 1000 AZN", right: "10 il sonra 100,000 AZN" },
-        { left: "Səyahət etmək (pulsuz)", right: "Villada yaşamaq (gedə bilməzsən)" },
+        { left: "İndi 1000 AZN", right: "10 il sonra 10,000 AZN" },
+        { left: "Çox pulun olsun, amma meymuna oxşa", right: "Vəhşicəsinə yaraşıqlı ol, amma acından günorta dur" },
+        { left: "1 milyon manat, amma heç vaxt istədiyini ala bilməmək", right: "Az pulun olsun, amma seçim azadlığı" },
         { left: "Hər gün yerdən 10 AZN tapmaq", right: "Ayda 1 dəfə 500 AZN tapmaq" },
+        { left: "Pul üçün sevmədiyin işi gör, amma çox qazan", right: "Sevdiyin işi gör, amma az qazan" },
+        { left: "Çox zəngin ol, amma heç kim səni səmimi sevməsin", right: "Az pulun olsun, amma hamı səni həqiqətən sevsin" },
+    { left: "1 milyon manat qazan, amma həyatının 10 ilini itir", right: "Heç nə qazanma, amma 10 il əlavə yaşa" },
+    { left: "Çox pulun olsun, amma ailənlə münasibətin korlansın", right: "Pulun az olsun, amma ailənlə çox yaxın ol" },
+    { left: "İstədiyin hər şeyi ala bil, amma heç nədən zövq ala bilmə", right: "Az şey ala bil, amma hər şeydən həzz al" },
+    { left: "Çox pul qazan, amma hər kəs səndən istifadə etməyə çalışsın", right: "Az pulun olsun, amma ətrafında səmimi insanlar olsun" },
+    { left: "Ömür boyu işləmədən pul qazan, amma heç vaxt azad hiss etmə", right: "Çox işləməli ol, amma öz həyatına nəzarət et" },
+    { left: "Çox varlı ol, amma hər qərarın başqaları tərəfindən idarə olunsun", right: "Az pulun olsun, amma bütün qərarları özün ver" },
+    { left: "1 milyon manatın olsun, amma heç kimə kömək edə bilmə", right: "Az pulun olsun, amma sevdiklərinə dəstək ola bil" },
+    { left: "Çox pulun olsun, amma daima itirmək qorxusu ilə yaşa", right: "Pulun az olsun, amma sabah üçün qorxun az olsun" },
+    { left: "Məşhur və varlı ol, amma şəxsi həyatın hamıya açıq olsun", right: "Normal gəlirin olsun, amma həyatın tam şəxsi qalsın" },
+    { left: "Pul üçün sevmədiyin işi gör, amma çox qazan", right: "Sevdiyin işi gör, amma az qazan" },
+    { left: "Çox pulun olsun, amma dostlarının çoxu saxta olsun", right: "Az pulun olsun, amma iki həqiqi dostun olsun" },
+    { left: "Hər ay çox pul qazan, amma heç vaxt istirahət edə bilmə", right: "Az qazan, amma istədiyin vaxt dincələ bil" },
+    { left: "Çox pulun olsun, amma keçmişdəki səhvlərin hamıya məlum olsun", right: "Az pulun olsun, amma reputasiyan təmiz qalsın" },
+    { left: "Varlı ol, amma həmişə tək hiss et", right: "Kasıb ol, amma heç vaxt tək qalmadığını hiss et" }
         { left: "Kasıb, amma xoşbəxt", right: "Zəngin, amma tənha" }
     ],
     yemek: [
